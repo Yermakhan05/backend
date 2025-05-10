6371eead341405550dd427c1c262f21b1a7f6fd0
yermakhankasymzhomart <k_yermakhan@kbtu.kz>
2025-05-11 02:37:57 +0500

    Deploy to Railway

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = 'django-insecure-cqz&zgian8fhh=bratpl5ey$#@phbfool574la%gru9b17q_3#'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
